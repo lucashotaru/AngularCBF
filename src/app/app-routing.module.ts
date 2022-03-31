efca4c58bdff1014dd63e673d38d8c347f48765f
LucasHotaru <lucashotaru01@gmail.com>
2022-03-31 16:43:25 -0300

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,10 @@
-import { AdministracaoComponent } from './pages/menu/administracao/administracao.component';
-import { EstatisticasJogadoresComponent } from './pages/menu/estatisticas/estatisticas-jogadores/estatisticas-jogadores.component';
-import { EstatisticasTimeComponent } from './pages/menu/estatisticas/estatisticasTime/estatisticasTime.component';
-import { TransmissoesComponent } from './pages/menu/transmissoes/transmissoes.component';
-import { NoticiasComponent } from './pages/menu/noticias/noticias.component';
-import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'tabelas/campeonatobrasileiro/serieA' },
-  { path: 'admin', pathMatch: 'full' , redirectTo: 'admin/uploadTabelas'},
-  {
-    path: '',
-    component: HomeComponent,
-    children: [
-      {
-        path: 'tabelas', loadChildren: () => import('./pages/menu/tabelas/tabelas.module').then(m => m.TabelasModule)
-      },
-      {
-        path: 'estatisticas/Time',
-        component: EstatisticasTimeComponent,
-      },
-      {
-        path: 'estatisticas/Jogadores',
-        component: EstatisticasJogadoresComponent,
-      },
-      {
-        path: 'noticias',
-        component: NoticiasComponent,
-      },
-      {
-        path: 'transmissoes',
-        component: TransmissoesComponent,
-      },
-      {
-        path: 'admin', component:AdministracaoComponent , loadChildren: () => import('./pages/menu/administracao/administracao.module').then(m => m.AdministracaoModule)
-      }
-    ],
-  },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
