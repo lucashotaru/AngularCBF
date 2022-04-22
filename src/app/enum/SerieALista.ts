623b0c408fa691effcc3223ff96427ff10c371f3
Lucas <lucashotaru01@gmail.com>
2022-04-21 23:20:53 -0300

    update

---
+++ src/app/enum/SerieALista.ts
@@ -0,0 +1,56 @@
+import { SeriesModel } from '../models/SeriesModel';
+
+export const SerieALista: SeriesModel[] = [
+  {
+    "valor": "serie-a",
+    "nome": "Série A",
+  },
+  {
+    "valor": "serie-b",
+    "nome": "Série B",
+  },
+  {
+    "valor": "serie-c",
+    "nome": "Série C",
+  },
+  {
+    "valor": "serie-d",
+    "nome": "Série D",
+  },
+  {
+    "valor": "feminino",
+    "nome": "Feminino",
+  },
+  {
+    "valor": "feminino-a1",
+    "nome": "Feminino A1",
+  },
+  {
+    "valor": "feminino-a2",
+    "nome": "Feminino A2",
+  },
+  {
+    "valor": "sub20",
+    "nome": "Sub-20",
+  },
+  {
+    "valor": "sub17",
+    "nome": "Sub-17",
+  },
+  {
+    "valor": "aspirantes",
+    "nome": "Aspirantes",
+  },
+  {
+    "valor": "feminino-sub18",
+    "nome": "Feminino Sub-18",
+  },
+  {
+    "valor": "feminino-sub17",
+    "nome": "Feminino Sub-17",
+  },
+  {
+    "valor": "feminino-sub16",
+    "nome": "Feminino Sub-16",
+  },
+];
