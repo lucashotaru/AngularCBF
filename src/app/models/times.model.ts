1e6acb20bb2101cd5ebb38b05640f981b9875942
leosima <leosima.lc@gmail.com>
2022-05-25 13:21:11 -0300

    Finalizando página com chart

--- src/app/models/times.model.ts
+++ src/app/models/times.model.ts
@@ -3,6 +3,7 @@ import { Temporada } from './temporada.models';
 export interface Times{
   id: number|string;
   nome: string;
+  titulo: string,
   ataque: number;
   meioCampo: number;
   defesa: number;
